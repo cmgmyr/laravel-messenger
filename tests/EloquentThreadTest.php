3fa4ccbe2f07dea3898b1f0da5515123d66e8e3f
Adam Wathan <adam.wathan@gmail.com>
2014-11-06 09:35:20 -0500

    Add 'latest message' test sample

--- tests/EloquentThreadTest.php
+++ tests/EloquentThreadTest.php
@@ -20,4 +20,19 @@ class EloquentThreadTest extends TestCase
 
         $this->assertEquals('Test Thread', $thread->subject);
     }
-} 
+
+    /** @test */
+    public function it_can_retrieve_the_latest_message()
+    {
+        $old_message = $this->faktory->build('message', [
+            'created_at' => new \DateTime('5 days ago')
+        ]);
+        $new_message = $this->faktory->build('message', [
+            'created_at' => new \DateTime,
+            'body' => 'This is the most recent message'
+        ]);
+        $thread = $this->faktory->create('thread');
+        $thread->messages()->saveMany([$old_message, $new_message]);
+        $this->assertEquals($new_message->body, $thread->latestMessage()->body);
+    }
+}
