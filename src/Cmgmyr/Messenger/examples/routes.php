c233aa4400f62305e7c7fdcfb9be15840fa591c3
Chris Gmyr <cmgmyr@gmail.com>
2014-11-08 08:31:25 -0500

    added example routes

---
+++ src/Cmgmyr/Messenger/examples/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::group(['prefix' => 'messages'], function()
+{
+    Route::get('/', ['as' => 'messages', 'uses' => 'MessagesController@index']);
+    Route::get('create', ['as' => 'messages.create', 'uses' => 'MessagesController@create']);
+    Route::post('/', ['as' => 'messages.store', 'uses' => 'MessagesController@store']);
+    Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
+    Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
+});
