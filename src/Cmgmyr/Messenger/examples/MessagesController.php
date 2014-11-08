c35cc559b9266e81056727899099b27946e8761e
Chris Gmyr <cmgmyr@gmail.com>
2014-11-08 08:30:32 -0500

    added example controller

---
+++ src/Cmgmyr/Messenger/examples/MessagesController.php
@@ -0,0 +1,147 @@
+<?php
+
+use Carbon\Carbon;
+use Cmgmyr\Messenger\Models\Thread;
+use Cmgmyr\Messenger\Models\Message;
+use Cmgmyr\Messenger\Models\Participant;
+
+class MessagesController extends BaseController
+{
+
+    /**
+     * Just for testing - the user should be logged in. In a real
+     * app, please use standard authentication practices
+     */
+    function __construct()
+    {
+        $user = User::find(1);
+        Auth::login($user);
+    }
+
+    /**
+     * Show all of the message threads to the user
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        // All threads, ignore deleted/archived participants
+        $threads = Thread::getAllLatest();
+
+        // All threads that user is participating in
+        // $threads = Thread::forUser();
+
+        // All threads that user is participating in, with new messages
+        // $threads = Thread::forUserWithNewMessages();
+
+        return View::make('messenger.index', compact('threads'));
+    }
+
+    /**
+     * Shows a message thread
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        $thread = Thread::findOrFail($id);
+        $users = User::whereNotIn('id', $thread->participantsUserIds())->get();
+
+        $thread->markAsRead();
+
+        return View::make('messenger.show', compact('thread', 'users'));
+    }
+
+    /**
+     * Creates a new message thread
+     *
+     * @return mixed
+     */
+    public function create()
+    {
+        $users = User::where('id', '!=', Auth::id())->get();
+
+        return View::make('messenger.create', compact('users'));
+    }
+
+    /**
+     * Stores a new message thread
+     *
+     * @return mixed
+     */
+    public function store()
+    {
+        $input = Input::all();
+
+        $thread = Thread::create(
+            [
+                'subject' => $input['subject'],
+            ]
+        );
+
+        // Message
+        Message::create(
+            [
+                'thread_id' => $thread->id,
+                'user_id'   => Auth::user()->id,
+                'body'      => $input['message'],
+            ]
+        );
+
+        // Sender
+        Participant::create(
+            [
+                'thread_id' => $thread->id,
+                'user_id'   => Auth::user()->id,
+                'last_read' => new Carbon
+            ]
+        );
+
+        // Recipients
+        if (Input::has('recipients')) {
+            $thread->addParticipants($input['recipients']);
+        }
+
+        return Redirect::to('');
+    }
+
+    /**
+     * Adds a new message to a current thread
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function update($id)
+    {
+        $thread = Thread::findOrFail($id);
+
+        $thread->activateAllParticipants();
+
+        // Message
+        Message::create(
+            [
+                'thread_id' => $thread->id,
+                'user_id'   => Auth::id(),
+                'body'      => Input::get('message'),
+            ]
+        );
+
+        // Add replier as a participant
+        $participant = Participant::firstOrCreate(
+            [
+                'thread_id' => $thread->id,
+                'user_id'   => Auth::user()->id
+            ]
+        );
+        $participant->last_read = new Carbon;
+        $participant->save();
+
+        // Recipients
+        if (Input::has('recipients')) {
+            $thread->addParticipants(Input::get('recipients'));
+        }
+
+        return Redirect::to('messages/' . $id);
+    }
+}
