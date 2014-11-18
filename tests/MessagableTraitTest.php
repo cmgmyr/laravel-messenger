58ac2a829f1e19e60c7d54a13fa3bf181bd7b844
Chris Gmyr <cmgmyr@gmail.com>
2014-11-18 07:17:36 -0500

    added test for messagable trait

---
+++ tests/MessagableTraitTest.php
@@ -0,0 +1,57 @@
+<?php namespace Cmgmyr\Messenger\tests;
+
+use Carbon\Carbon;
+use Cmgmyr\Messenger\Traits\Messagable;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class MessagableTraitTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        Eloquent::unguard();
+    }
+
+    /** @test */
+    public function it_should_get_all_threads_with_new_messages()
+    {
+        $user = User::create(
+            [
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'email' => 'john@example.com',
+                'notify' => 'y'
+            ]
+        );
+
+        $thread = $this->faktory->create('thread');
+        $user_1 = $this->faktory->build('participant', ['user_id' => $user->id, 'last_read' => Carbon::yesterday()]);
+        $user_2 = $this->faktory->build('participant', ['user_id' => 2]);
+        $thread->participants()->saveMany([$user_1, $user_2]);
+
+        $message_1 = $this->faktory->build('message', ['user_id' => 2]);
+        $thread->messages()->saveMany([$message_1]);
+
+        $thread2 = $this->faktory->create('thread');
+        $user_1b = $this->faktory->build('participant', ['user_id' => 3, 'last_read' => Carbon::yesterday()]);
+        $user_2b = $this->faktory->build('participant', ['user_id' => 2]);
+        $thread2->participants()->saveMany([$user_1b, $user_2b]);
+
+        $message_1b = $this->faktory->build('message', ['user_id' => 2]);
+        $thread2->messages()->saveMany([$message_1b]);
+
+        $threads = $user->threadsWithNewMessages();
+        $this->assertEquals(1, $threads[0]);
+
+        $this->assertEquals(1, $user->newMessagesCount());
+    }
+}
+
+class User extends Eloquent
+{
+    use Messagable;
+
+    protected $table = 'users';
+
+    protected $fillable = ['first_name', 'last_name', 'email', 'notify'];
+}
