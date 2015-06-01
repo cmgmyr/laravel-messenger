5e2a291789a5f939249274de9765f941c5543992
Chris Gmyr <cmgmyr@gmail.com>
2015-06-01 14:51:21 -0400

    added creator() method to Thread model

--- src/Cmgmyr/Messenger/examples/views/index.blade.php
+++ src/Cmgmyr/Messenger/examples/views/index.blade.php
@@ -12,6 +12,7 @@
         <div class="media alert {{$class}}">
             <h4 class="media-heading">{{link_to('messages/' . $thread->id, $thread->subject)}}</h4>
             <p>{{$thread->latestMessage->body}}</p>
+            <p><small><strong>Creator:</strong> {{ $thread->creator()->name }}</small></p>
             <p><small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small></p>
         </div>
         @endforeach
