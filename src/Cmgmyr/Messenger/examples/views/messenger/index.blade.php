243aa47b27491cd8ff94153b65004be61a95e8de
Chris Gmyr <cmgmyr@gmail.com>
2016-08-26 17:32:40 -0400

    update example files to use correct syntax

--- src/Cmgmyr/Messenger/examples/views/messenger/index.blade.php
+++ src/Cmgmyr/Messenger/examples/views/messenger/index.blade.php
@@ -3,17 +3,17 @@
 @section('content')
     @if (Session::has('error_message'))
         <div class="alert alert-danger" role="alert">
-            {!! Session::get('error_message') !!}
+            {{ Session::get('error_message') }}
         </div>
     @endif
     @if($threads->count() > 0)
         @foreach($threads as $thread)
         <?php $class = $thread->isUnread($currentUserId) ? 'alert-info' : ''; ?>
-        <div class="media alert {!!$class!!}">
+        <div class="media alert {{ $class }}">
             <h4 class="media-heading">{!! link_to('messages/' . $thread->id, $thread->subject) !!}</h4>
-            <p>{!! $thread->latestMessage->body !!}</p>
-            <p><small><strong>Creator:</strong> {!! $thread->creator()->name !!}</small></p>
-            <p><small><strong>Participants:</strong> {!! $thread->participantsString(Auth::id()) !!}</small></p>
+            <p>{{ $thread->latestMessage->body }}</p>
+            <p><small><strong>Creator:</strong> {{ $thread->creator()->name }}</small></p>
+            <p><small><strong>Participants:</strong> {{ $thread->participantsString(Auth::id()) }}</small></p>
         </div>
         @endforeach
     @else
