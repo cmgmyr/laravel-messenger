36155b36f5da57633c475fb27455fc27aa821994
a-komarev <ell@cybercog.su>
2016-10-03 19:43:43 +0300

    Added Schema facade dependency to be up to date with L5.3 migrations.

--- src/migrations/2014_10_28_175635_create_threads_table.php
+++ src/migrations/2014_10_28_175635_create_threads_table.php
@@ -3,6 +3,7 @@
 use Cmgmyr\Messenger\Models\Models;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateThreadsTable extends Migration
 {
