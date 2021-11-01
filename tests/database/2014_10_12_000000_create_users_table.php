68188d1527bacafd36160b4b79e2b4906316ddda
Chris Gmyr <cmgmyr@gmail.com>
2021-11-01 10:48:28 -0400

    rm docblocks

--- tests/database/2014_10_12_000000_create_users_table.php
+++ tests/database/2014_10_12_000000_create_users_table.php
@@ -6,12 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
@@ -24,12 +19,7 @@ class CreateUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
