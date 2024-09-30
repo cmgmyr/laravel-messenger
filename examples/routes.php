f907ae2bc0d0eec9f279615afdb1c7a9ae9146e6
shailesh <shaileshmladumor@gmail.com>
2024-09-30 14:22:42 +0530

    fix(routes): correct route method post to get

--- examples/routes.php
+++ examples/routes.php
@@ -5,6 +5,6 @@ Route::group(['prefix' => 'messages'], function () {
     Route::get('/', [MessagesController::class, 'index'])->name('messages');
     Route::get('/create', [MessagesController::class, 'create'])->name('messages.create');
     Route::post('/', [MessagesController::class, 'store'])->name('messages.store');
-    Route::post('/{id}', [MessagesController::class, 'show'])->name('messages.show');
+    Route::get('/{id}', [MessagesController::class, 'show'])->name('messages.show');
     Route::put('/{id}', [MessagesController::class, 'update'])->name('messages.update');
 });
